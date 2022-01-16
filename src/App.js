a9861077d5bf04aaf1572cfb0fdd7583ec0a6c29
minhazpro3 <minhazfr8@gmail.com>
2022-01-16 21:04:05 +0600

    redux-couter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber,decNumber } from './actions';
 import './App.css';
 
 function App() {
+
+
+  const myState = useSelector((state)=> state.changeTheNumber)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Welcome to REDUX</h2>
+    <div className="main-div">
+      
+
+      <div className="container">
+  
+      <h1>Increment/Decrement counter</h1>
+      <h4>using React and Redux</h4>
+      
+      <div className="quantity">
+        <button className="quantity__minus" title="Decrement" onClick={ () => dispatch(decNumber())}><span>-</span></button>
+        <input name="quantity" type="text" className="quantity__input" value={myState} />
+        <button className="quantity__plus" title="Increment"  onClick={ () => dispatch(incNumber(5))}><span>+</span></button>
+      </div>
+  
+          </div>
+        </div>
     </div>
   );
 }
